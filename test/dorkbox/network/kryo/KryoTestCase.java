b1934a5b2dfdb19026c4c7d99fbe948d9a3bb74c
nathan <nathan@dorkbox.com>
2016-03-07 01:06:43 +0100

    Updated file licensing

--- test/dorkbox/network/kryo/KryoTestCase.java
+++ test/dorkbox/network/kryo/KryoTestCase.java
@@ -1,4 +1,22 @@
-package dorkbox.network.kryo;
+/* Copyright (c) 2008, Nathan Sweet
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the distribution.
+ * - Neither the name of Esoteric Software nor the names of its contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */package dorkbox.network.kryo;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
@@ -39,15 +57,15 @@ abstract public class KryoTestCase extends TestCase {
 	protected boolean supportsCopy;
 
 	static interface StreamFactory {
-		public Output createOutput(OutputStream os);
+		Output createOutput(OutputStream os);
 
-		public Output createOutput(OutputStream os, int size);
+		Output createOutput(OutputStream os, int size);
 
-		public Output createOutput(int size, int limit);
+		Output createOutput(int size, int limit);
 
-		public Input createInput(InputStream os, int size);
+		Input createInput(InputStream os, int size);
 
-		public Input createInput(byte[] buffer);
+		Input createInput(byte[] buffer);
 	}
 
 	@Override
