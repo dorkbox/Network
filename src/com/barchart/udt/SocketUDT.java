63d8d0665349e42810f2ec34c55055d5faa8734f
nathan <nathan@dorkbox.com>
2016-03-19 12:40:34 +0100

    Changed UDT dist name to netty-UDT...

--- src/com/barchart/udt/SocketUDT.java
+++ src/com/barchart/udt/SocketUDT.java
@@ -166,7 +166,7 @@ class SocketUDT {
 
                 sourceFileName = packageName + "/natives/" + osName + "/" + sourceFileName;
 
-                NativeLoader.loadLibrary(sourceFileName, "networkUDT_", TypeUDT.class);
+                NativeLoader.loadLibrary(sourceFileName, "libnetty-UDT-", TypeUDT.class);
 
                 log.info("Release libraries loaded.");
                 loaded = true;
