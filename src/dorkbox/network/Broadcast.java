f6ebbc108014dbac99cf9696d2d45e7575e3536f
nathan <nathan@dorkbox.com>
2016-04-28 16:46:18 +0200

    Updated version

--- src/dorkbox/network/Broadcast.java
+++ src/dorkbox/network/Broadcast.java
@@ -48,7 +48,7 @@ class Broadcast {
      */
     public static
     String getVersion() {
-        return "1.18";
+        return "1.19";
     }
 
     /**
--- src/dorkbox/network/Server.java
+++ src/dorkbox/network/Server.java
@@ -63,7 +63,7 @@ class Server<C extends Connection> extends EndPointServer<C> {
      */
     public static
     String getVersion() {
-        return "1.18";
+        return "1.19";
     }
 
     /**
