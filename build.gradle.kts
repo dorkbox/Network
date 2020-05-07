661957cc65427306297de232f503d5a45c6dd7f6
nathan <nathan@dorkbox.com>
2020-05-07 23:07:20 +0200

    Updated version from 4.0 to 4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,7 +50,7 @@ object Extras {
     // set for the project
     const val description = "Encrypted, high-performance, and event-driven/reactive network stack for Java 11+"
     const val group = "com.dorkbox"
-    const val version = "4.0"
+    const val version = "4.1"
 
     // set as project.ext
     const val name = "Network"
