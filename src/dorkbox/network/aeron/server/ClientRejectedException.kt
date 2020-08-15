1b796971d314b9c429a59e816a5f3179a1b07ca1
nathan <nathan@dorkbox.com>
2020-08-15 13:13:40 +0200

    Changed ClientRejectedException hierarchy

--- src/dorkbox/network/aeron/server/ClientRejectedException.kt
+++
@@ -1,6 +0,0 @@
-package dorkbox.network.aeron.server
-
-/**
- * The server rejected this client when it tried to connect.
- */
-class ClientRejectedException(message: String, cause: Throwable? = null) : ServerException(message, cause)
