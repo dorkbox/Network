e61f3e471171978a08e8ce630e7c86c01b588902
Robinson <robinson@dorkbox.com>
2022-04-04 23:22:04 +0200

    Moved multiconnect to it's own test file

--- test/dorkboxTest/network/DisconnectReconnectTest.kt
+++ test/dorkboxTest/network/DisconnectReconnectTest.kt
@@ -5,16 +5,11 @@ import dorkbox.network.Server
 import dorkbox.network.aeron.AeronDriver
 import dorkbox.network.connection.Connection
 import kotlinx.atomicfu.atomic
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Test
 import java.io.IOException
-import kotlin.time.Duration.Companion.seconds
 
 class DisconnectReconnectTest : BaseTest() {
     private val reconnectCount = atomic(0)
@@ -123,55 +118,6 @@ class DisconnectReconnectTest : BaseTest() {
 
 
         waitForThreads(0)
-
-        System.err.println("Connection count (after reconnecting) is: " + reconnectCount.value)
-        Assert.assertEquals(4, reconnectCount.value)
-    }
-
-    @Test
-    fun multiConnectClient() {
-        // clients first, so they try to connect to the server at (roughly) the same time
-        val config = clientConfig()
-
-        val client1: Client<Connection> = Client(config)
-        val client2: Client<Connection> = Client(config)
-
-        addEndPoint(client1)
-        addEndPoint(client2)
-        client1.onDisconnect {
-            logger.error("Disconnected 1!")
-        }
-        client2.onDisconnect {
-            logger.error("Disconnected 2!")
-        }
-
-        GlobalScope.launch {
-            client1.connect(LOCALHOST)
-        }
-
-        GlobalScope.launch {
-            client2.connect(LOCALHOST)
-        }
-
-        println("Starting server...")
-
-        run {
-            val configuration = serverConfig()
-
-            val server: Server<Connection> = Server(configuration)
-            addEndPoint(server)
-            server.bind()
-
-            server.onConnect {
-                logger.error("Disconnecting after 10 seconds.")
-                delay(10.seconds)
-
-                logger.error("Disconnecting....")
-                close()
-            }
-        }
-
-        waitForThreads()
 
         System.err.println("Connection count (after reconnecting) is: " + reconnectCount.value)
         Assert.assertEquals(4, reconnectCount.value)
