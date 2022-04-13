6f1fdfcff95086499f14fc7c91046f62c0f5701d
Robinson <robinson@dorkbox.com>
2022-04-13 16:22:47 +0200

    Updated version

--- src/dorkbox/network/Client.kt
+++ src/dorkbox/network/Client.kt
@@ -69,7 +69,7 @@ open class Client<CONNECTION : Connection>(
         /**
          * Gets the version number.
          */
-        const val version = "5.10"
+        const val version = "5.11"
 
         /**
          * Checks to see if a client (using the specified configuration) is running.
--- src/dorkbox/network/Server.kt
+++ src/dorkbox/network/Server.kt
@@ -65,7 +65,7 @@ open class Server<CONNECTION : Connection>(
         /**
          * Gets the version number.
          */
-        const val version = "5.10"
+        const val version = "5.11"
 
         /**
          * Checks to see if a server (using the specified configuration) is running.
