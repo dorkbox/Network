08ecaaf1c7fcbada6695af22a0b826bd92c1a66e
Robinson <robinson@dorkbox.com>
2022-05-30 02:43:41 +0200

    Code cleanup

--- src/dorkbox/network/coroutines/SuspendWaiter.kt
+++ src/dorkbox/network/coroutines/SuspendWaiter.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.channels.Channel
 
 // this is bi-directional waiting. The method names to not reflect this, however there is no possibility of race conditions w.r.t. waiting
 // https://kotlinlang.org/docs/reference/coroutines/channels.html
-internal data class SuspendWaiter(private val channel: Channel<Unit> = Channel()) {
+internal data class SuspendWaiter(private val channel: Channel<Int> = Channel()) {
     // "receive' suspends until another coroutine invokes "send"
     // and
     // "send" suspends until another coroutine invokes "receive".
@@ -16,7 +16,7 @@ internal data class SuspendWaiter(private val channel: Channel<Unit> = Channel()
     }
     suspend fun doNotify() {
         try {
-            channel.send(Unit)
+            channel.send(0)
         } catch (ignored: Exception) {
         }
     }
