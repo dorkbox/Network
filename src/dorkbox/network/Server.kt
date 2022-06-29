aea56477fa6b4d6d22a253b8dc2b493d326f9b80
Robinson <robinson@dorkbox.com>
2022-06-29 21:56:35 +0200

    Updated version

--- src/dorkbox/network/Server.kt
+++ src/dorkbox/network/Server.kt
@@ -123,7 +123,7 @@ open class Server<CONNECTION : Connection>(
         /**
          * Gets the version number.
          */
-        const val version = "5.25"
+        const val version = "5.26"
 
         /**
          * Checks to see if a server (using the specified configuration) is running.
