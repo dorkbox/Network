2c1155943efbde90f6f98a6d964fc17b810a8e6d
Robinson <robinson@dorkbox.com>
2022-07-03 13:13:44 +0200

    Updated library

--- build.gradle.kts
+++ build.gradle.kts
@@ -147,7 +147,7 @@ dependencies {
     api("com.dorkbox:ByteUtilities:1.5")
     api("com.dorkbox:Collections:1.1")
     api("com.dorkbox:MinLog:2.4")
-    api("com.dorkbox:NetworkDNS:2.6")
+    api("com.dorkbox:NetworkDNS:2.7")
     api("com.dorkbox:NetworkUtils:2.18")
     api("com.dorkbox:ObjectPool:4.0")
     api("com.dorkbox:OS:1.0")
