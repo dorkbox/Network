b4b45ba333b7bba4b05569f3443a8f7717c884b6
Robinson <robinson@dorkbox.com>
2022-08-04 03:41:42 +0200

    commented out/removed unnecessary dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -161,9 +161,7 @@ dependencies {
     // https://github.com/real-logic/aeron
     val aeronVer = "1.38.1"
     api("io.aeron:aeron-all:$aeronVer")
-//    api("io.aeron:aeron-client:$aeronVer")
-//    api("io.aeron:aeron-driver:$aeronVer")
-    api("org.agrona:agrona:1.16.0")
+//    api("org.agrona:agrona:1.16.0") // sources for this isn't included in aeron-all!
 
     // https://github.com/EsotericSoftware/kryo
     api("com.esotericsoftware:kryo:5.3.0") {
