115e63edf52bfabe43ba4d5d013815ffa12b9349
Robinson <robinson@dorkbox.com>
2023-06-22 11:47:02 +0200

    Added more exception types

---
+++ src/dorkbox/network/exceptions/PingException.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023 dorkbox, llc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dorkbox.network.exceptions
+
+/**
+ * The type of exceptions raised for ping errors
+ */
+open class PingException : Exception {
+    /**
+     * Create an exception.
+     *
+     * @param message The message
+     */
+    constructor(message: String) : super(message)
+
+    /**
+     * Create an exception.
+     *
+     * @param cause The cause
+     */
+    constructor(cause: Throwable) : super(cause)
+
+    /**
+     * Create an exception.
+     *
+     * @param message The message
+     *  @param cause The cause
+     */
+    constructor(message: String, cause: Throwable?) : super(message, cause)
+}
