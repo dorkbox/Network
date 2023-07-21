ee558e666db15fc39a3eb24d9bf4a84a76556c1e
Robinson <robinson@dorkbox.com>
2023-07-21 21:16:49 +0200

    Added additional idle strategies

---
+++ src/dorkbox/network/aeron/CoroutineBusySpinIdleStrategy.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2023 dorkbox, llc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dorkbox.network.aeron
+
+import org.agrona.concurrent.BusySpinIdleStrategy
+import org.agrona.concurrent.IdleStrategy
+import org.agrona.hints.ThreadHints
+
+class CoroutineBusySpinIdleStrategy : CoroutineIdleStrategy {
+
+    companion object {
+        const val ALIAS = "spin"
+        val INSTANCE = CoroutineBusySpinIdleStrategy()
+    }
+
+    override suspend fun idle(workCount: Int) {
+        if (workCount <= 0) {
+            ThreadHints.onSpinWait()
+        }
+    }
+
+    override suspend fun idle() {
+        ThreadHints.onSpinWait()
+    }
+
+    override fun reset() {}
+    override fun alias(): String {
+        return "spin"
+    }
+
+    override fun clone(): CoroutineIdleStrategy {
+        return CoroutineBusySpinIdleStrategy()
+    }
+
+    override fun cloneToNormal(): IdleStrategy {
+        return BusySpinIdleStrategy()
+    }
+
+    override fun toString(): String {
+        return "CoroutineBusySpinIdleStrategy{alias=spin}"
+    }
+}
