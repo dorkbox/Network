342abd495d0e8efbfd7c3d0da1c5c9bbaabffcec
Robinson <robinson@dorkbox.com>
2023-08-06 01:00:29 -0600

    updated deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -163,19 +163,19 @@ shadowJar.apply {
 
 dependencies {
     api("org.jetbrains.kotlinx:atomicfu:0.21.0")
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 
     // https://github.com/dorkbox
-    api("com.dorkbox:ByteUtilities:1.12")
+    api("com.dorkbox:ByteUtilities:1.13")
     api("com.dorkbox:ClassUtils:1.1")
-    api("com.dorkbox:Collections:2.2")
+    api("com.dorkbox:Collections:2.3")
     api("com.dorkbox:JNA:1.0")
     api("com.dorkbox:MinLog:2.5")
     api("com.dorkbox:NetworkDNS:2.10")
     api("com.dorkbox:NetworkUtils:2.22")
     api("com.dorkbox:OS:1.6")
     api("com.dorkbox:Serializers:2.9")
-    api("com.dorkbox:Storage:1.5")
+    api("com.dorkbox:Storage:1.6")
     api("com.dorkbox:Updates:1.1")
     api("com.dorkbox:Utilities:1.44")
 
@@ -228,7 +228,7 @@ dependencies {
     testImplementation("ch.qos.logback:logback-classic:1.4.5")
     testImplementation("io.aeron:aeron-all:$aeronVer")
 
-    testImplementation("com.dorkbox:Config:2.0")
+    testImplementation("com.dorkbox:Config:2.1")
 }
 
 publishToSonatype {
