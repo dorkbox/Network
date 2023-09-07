daf289c7b746e40696d0f72ab6b67670bd535874
Robinson <robinson@dorkbox.com>
2023-09-07 18:15:10 +0200

    updated deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -176,7 +176,7 @@ dependencies {
     api("com.dorkbox:NetworkUtils:2.23")
     api("com.dorkbox:OS:1.8")
     api("com.dorkbox:Serializers:2.9")
-    api("com.dorkbox:Storage:1.8")
+    api("com.dorkbox:Storage:1.10")
     api("com.dorkbox:Updates:1.1")
     api("com.dorkbox:Utilities:1.46")
 
